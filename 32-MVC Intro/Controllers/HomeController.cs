5134ea5e288773a471a402652975e8b1cae81bf5
rauvhhx <raufmemmedli4b@gmail.com>
2025-12-09 07:36:28 +0400

    edit

---
+++ 32-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            //return Content("APA102");
+
+            //var student = new JsonResult(
+            //    new
+            //    {
+            //        Id = 1,
+            //        Name = "Elvin",
+            //        Surname = "Aliyev"
+            //    }
+            //    );
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+
+
+            //return id;
+
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ 32-MVC Intro/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
